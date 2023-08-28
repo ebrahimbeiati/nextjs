761ea761403aa46b15425c6d374db3c3b0223b3f
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-08-28 18:00:07 +0100

    eventId.js

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -2,6 +2,8 @@ import { useRouter } from "next/router";
 import { getEventById } from "@/dummy-data";
 import { Fragment } from "react";
 import EventSummary from "@/components/event-detail/event-summary";
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
 const EventDetailPage = () => {
     const router = useRouter();
     const eventId = router.query.eventId;
@@ -12,7 +14,17 @@ const EventDetailPage = () => {
     
 
   return <Fragment>
-    <EventSummary/>
+    <EventSummary title={event.title}/>
+  
+    <EventLogistics
+    date={event.date}
+    address={event.location}
+    image={event.image}
+    imageAlt={event.title}
+    /> 
+     <EventContent>
+    <p>{event.description}</p>
+    </EventContent>
   </Fragment>;
 }
 
